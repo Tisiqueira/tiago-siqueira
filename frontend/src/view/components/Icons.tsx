26975af8d795f7acf9eed348c72cd68784a38330
Tisiqueira <tiago.m.siqueira@outlook.com>
2023-08-24 00:44:40 -0300

    change navbar

--- frontend/src/view/components/Icons.tsx
+++ frontend/src/view/components/Icons.tsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
-import { FaReact, FaNodeJs, FaHtml5, FaJs, FaGithub } from 'react-icons/fa6'
+import { FaReact, FaNodeJs, FaHtml5, FaJs, FaGithub,FaMicrosoft } from 'react-icons/fa6'
+import {SiMysql, SiFigma} from 'react-icons/si'
 
 
 export function IconCarousel() {
-  const icons = [<FaReact size={40} />, <FaNodeJs size={40} />, <FaHtml5 size={40} />, <FaJs size={40} />,<FaGithub size={40} />];
+  const icons = [<FaReact size={40} />, <FaNodeJs size={40} />, <FaHtml5 size={40} />, <FaJs size={40} />,<FaGithub size={40} />, < FaMicrosoft size={ 40} />,
+  <SiMysql size= {40} />, <SiFigma size={40}/>];
   const [currentIndex, setCurrentIndex] = useState(0);
 
   useEffect(() => {
@@ -15,11 +17,11 @@ export function IconCarousel() {
   }, []);
 
   return (
-    <div className="flex items-center justify-center h-[10vh]">
+    <div className="flex items-center justify-start h-[10vh]">
       <div className="icon-carousel flex items-center">
         {icons.map((icon, index) => (
           <div key={index} className={`icon-container ${currentIndex === index ? 'active transform scale-125 opacity-100' : 'opacity-50'}  inline-flex items-center justify-center w-[60px]
-          h-[10px] my-0 mx-[10px] rounded-[50%]  `}>
+          h-[5px] my-0 mx-0 rounded-[50%]  `}>
             {icon}
           </div>
         ))}
