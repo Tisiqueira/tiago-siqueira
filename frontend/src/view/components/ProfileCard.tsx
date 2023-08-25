769c53435071aecd4ced2de72f3af880c4d87830
Tisiqueira <tiago.m.siqueira@outlook.com>
2023-08-24 23:36:40 -0300

    change rotate icons

--- frontend/src/view/components/ProfileCard.tsx
+++ frontend/src/view/components/ProfileCard.tsx
@@ -1,4 +1,62 @@
+import { useState } from "react";
+
 export function ProfileCard(){
+
+    const [isHoveredInstagram, setIsHoveredInstagram] = useState(false);
+
+    const handleMouseEnterInstagram = () => {
+        setIsHoveredInstagram(true);
+    };
+
+    const handleMouseLeaveInstagram = () => {
+        setIsHoveredInstagram(false);
+
+    };
+
+    const [isHoveredTwitter, setIsHoveredTwitter] = useState(false);
+
+    const handleMouseEnterTwitter = () => {
+        setIsHoveredTwitter(true);
+    };
+
+    const handleMouseLeaveTwitter = () => {
+        setIsHoveredTwitter(false);
+
+    };
+
+    const [isHoveredThreads, setIsHoveredThreads] = useState(false);
+
+    const handleMouseEnterThreads = () => {
+        setIsHoveredThreads(true);
+    };
+
+    const handleMouseLeaveThreads = () => {
+        setIsHoveredThreads(false);
+
+    };
+
+    const [isHoveredLinkedin, setIsHoveredLinkedin] = useState(false);
+
+    const handleMouseEnterLinkedin = () => {
+        setIsHoveredLinkedin(true);
+    };
+
+    const handleMouseLeaveLinkedin = () => {
+        setIsHoveredLinkedin(false);
+
+    };
+
+    const rotationStyleLinkedin = isHoveredLinkedin ? 'rotateY(360deg)' : 'rotateY(0deg)';
+
+
+    const rotationStyleThreads = isHoveredThreads ? 'rotateY(360deg)' : 'rotateY(0deg)';
+
+    const rotationStyleTwitter = isHoveredTwitter ? 'rotateY(360deg)' : 'rotateY(0deg)';
+
+
+    const rotationStyleInstagram = isHoveredInstagram ? 'rotateY(360deg)' : 'rotateY(0deg)';
+
+
     return(
         // <div 
         //     className="image rounded-2xl h-[90%] w-[68%] ml-16 bg-black
@@ -19,12 +77,20 @@ export function ProfileCard(){
                     />
                 </div>
                 <ul className="social-icons absolute flex gap-3 z-[3] bottom-[164px] opacity-0 group-hover:opacity-100 transition-opacity">
-                    <li className="list-none">
-                        <a href="#instagram" title="Instagram" className="relative grid place-items-center bg-black text-[23px] font-bold w-10 h-10 border border-solid border-[#CDCDCD]
-                                rounded  transition-[.2s] translate-y-8 hover:bg-white">
-                             <svg className="bg-black transition-[.2s] hover:bg-white hover:transition-[0.9s] hover:transform-[rotateY(360deg)]" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                     viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" 
-                                     stroke-linecap="round" stroke-linejoin="round"
+                    <li className="list-none ">
+                        <a href="#instagram" title="Instagram" className="relative grid place-items-center  text-[23px] font-bold w-10 h-10 border border-solid border-[#CDCDCD]
+                                rounded  transition-[.1s] translate-y-8 hover:bg-black hover:text-white bg-white text-black ">
+                             <svg className="bg-[currenteColor]  text-[currentColor] hover:bg-black hover:text-white transition-[.2s]  hover:transition-[0.9s]" 
+                                    style={{
+                                        transform: rotationStyleInstagram,
+                                        transition: 'transform 0.9s',
+                                      }}
+                                      onMouseEnter={handleMouseEnterInstagram}
+                                      onMouseLeave={handleMouseLeaveInstagram}
+                                    
+                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" 
+                                    stroke-linecap="round" stroke-linejoin="round"
                                 >
                                     <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
                                     <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
@@ -33,11 +99,17 @@ export function ProfileCard(){
                         </a>
                     </li>
                     <li className="list-none">
-                        <a href="#twitter" title="Twitter" className="relative grid place-items-center bg-black text-[23px] font-bold w-10 h-10 border border-solid border-[#CDCDCD]
-                                rounded  transition-[.2s] translate-y-8 hover:bg-white">
+                        <a href="#twitter" title="Twitter" className="relative grid place-items-center text-[23px] font-bold w-10 h-10 border border-solid border-[#CDCDCD]
+                                rounded  transition-[.2s] translate-y-8 hover:bg-black hover:text-white bg-white text-black">
                             <svg  xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
                                      viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" 
-                                     stroke-linecap="round" stroke-linejoin="round" className="bg-black transition-[.2s] hover:bg-white hover:transition-[0.9s] hover:transform-[rotateY(360deg)]"
+                                     stroke-linecap="round" stroke-linejoin="round" className="hover:bg-black transition-[.2s] hover:text-white hover:transition-[0.9s] "
+                                     style={{
+                                        transform: rotationStyleTwitter,
+                                        transition: 'transform 0.9s',
+                                      }}
+                                      onMouseEnter={handleMouseEnterTwitter}
+                                      onMouseLeave={handleMouseLeaveTwitter}
                                 >
                                     <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 
                                             10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5
@@ -48,11 +120,17 @@ export function ProfileCard(){
                         </a>
                     </li>
                     <li className="list-none">
-                        <a href="#threads" title="Threads" className="relative grid place-items-center bg-black text-[23px] font-bold w-10 h-10 border border-solid border-[#CDCDCD]
-                                rounded  transition-[.2s] translate-y-8 hover:bg-white">
-                            <svg className="bg-black transition-[.2s] hover:bg-white hover:transition-[0.9s] hover:transform-[rotateY(360deg)]" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                        <a href="#threads" title="Threads" className="relative grid place-items-center  text-[23px] font-bold w-10 h-10 border border-solid border-[#CDCDCD]
+                                rounded  transition-[.2s] translate-y-8 hover:bg-black hover:text-white bg-white text-black">
+                            <svg className="hover:bg-black transition-[.2s] hover:text-white hover:transition-[0.9s] " xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                      viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" 
                                      fill="none" stroke-linecap="round" stroke-linejoin="round"
+                                     style={{
+                                        transform: rotationStyleThreads,
+                                        transition: 'transform 0.9s',
+                                      }}
+                                      onMouseEnter={handleMouseEnterThreads}
+                                      onMouseLeave={handleMouseLeaveThreads}
                                 >
                                     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                                     <path d="M19 7.5c-1.333 -3 -3.667 -4.5 -7 -4.5c-5 0 -8 2.5 -8 9s3.5 9 8 9s7 -3 7
@@ -64,11 +142,17 @@ export function ProfileCard(){
                         </a>
                     </li>
                     <li className="list-none">
-                        <a href="#linkedin" title="Linkedin" className="relative grid place-items-center bg-black text-[23px] font-bold w-10 h-10 border border-solid border-[#CDCDCD]
-                                rounded  transition-[.2s] translate-y-8 hover:bg-white">
-                             <svg  className="bg-[#2C2C2C] transition-[.2s] hover:bg-white hover:transition-[0.9s] hover:transform-[rotateY(360deg)]" xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
+                        <a href="#linkedin" title="Linkedin" className="relative grid place-items-center text-[23px] font-bold w-10 h-10 border border-solid border-[#CDCDCD]
+                                rounded  transition-[.2s] translate-y-8 hover:bg-black hover:text-white bg-white text-black">
+                             <svg  className="hover:bg-black transition-[.2s] hover:text-white hover:transition-[0.9s] " xmlns="http://www.w3.org/2000/svg" width="24" height="24" 
                                      viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
                                      fill="none" stroke-linecap="round" stroke-linejoin="round"
+                                     style={{
+                                        transform: rotationStyleLinkedin,
+                                        transition: 'transform 0.10s',
+                                      }}
+                                      onMouseEnter={handleMouseEnterLinkedin}
+                                      onMouseLeave={handleMouseLeaveLinkedin}
                                 >
                                     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                                     <path d="M4 4m0 2a2 2 0 0 1 2 -2h12a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-12a2 
