c741b3b3234c0a6c183f9d786e23540d9ffbf40e
Tiago Siqueira <tiago.m.siqueira@outlook.com>
2024-07-02 00:44:42 +0000

    Add useState

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Tecnology from "./Tecnology";
 
-const tecnologies = [
-  { title: "HTML", subtitle: "Experiência de mais de 3 anos" },
-  { title: "CSS3", subtitle: "Experiência de mais de 3 anos" },
-  { title: "Javascript", subtitle: "Experiência de mais de 3 anos" },
-  { title: "React", subtitle: "Experiência de 1 ano" },
-  { title: "Node", subtitle: "Experiência de 1 ano" },
-];
-
 function App() {
+  const [tecnologies, setTecnologies] = useState([
+    {
+      id: Math.random(),
+      title: "HTML",
+      subtitle: "Experiência de mais de 3 anos",
+    },
+    {
+      id: Math.random(),
+      title: "CSS3",
+      subtitle: "Experiência de mais de 3 anos",
+    },
+    {
+      id: Math.random(),
+      title: "Javascript",
+      subtitle: "Experiência de mais de 3 anos",
+    },
+    { id: Math.random(), title: "React", subtitle: "Experiência de 1 ano" },
+    { id: Math.random(), title: "Node", subtitle: "Experiência de 1 ano" },
+  ]);
+
+  function handleRefresh() {
+    setTecnologies((prevState) => [
+      ...prevState,
+      {
+        id: Math.random(),
+        title: `Title${prevState.length + 1}`,
+        subtitle: `Title${prevState.length + 1}`,
+      },
+    ]);
+  }
+
   return (
     <>
       <Header title="Tiago Alves Mastrogiacomo Siqueira">
         <h2>Tecnologias</h2>
+        <button onClick={handleRefresh}>Add</button>
       </Header>
 
       <hr />
 
       {tecnologies.map((tec) => (
-        <Tecnology key={tec.title} title={tec.title} subtitle={tec.subtitle} />
+        <Tecnology key={tec.id} title={tec.title} subtitle={tec.subtitle} />
       ))}
     </>
   );
