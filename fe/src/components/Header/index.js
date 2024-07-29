c260bbcf3115248cc99d0d1513698efe4fa3f004
Tiago Siqueira <tiago.m.siqueira@outlook.com>
2024-07-29 04:02:30 +0000

    create a logo and menu

--- fe/src/components/Header/index.js
+++ fe/src/components/Header/index.js
@@ -1,3 +1,15 @@
+import { Navigation, Container } from "./styles";
+
+import logoDark from "../../assets/images/LogoDark.svg";
+import menuDark from "../../assets/images/MenuDark.svg";
+
 export default function Header() {
-  return "Header";
+  return (
+    <Navigation>
+      <Container>
+        <img src={menuDark} alt="Menu" className="menu" />
+        <img src={logoDark} alt="TiSiqueira" />
+      </Container>
+    </Navigation>
+  );
 }
---
+++ fe/src/components/Header/styles.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Navigation = styled.nav`
+  display: block;
+  margin: 16px 8px;
+  width: 100%;
+`;
+
+export const Container = styled.div`
+  margin: 8px;
+  padding: 0 16px;
+  display: flex;
+  align-item: center;
+  justify-content: space-between;
+  width: 100%;
+
+  .menu {
+    width: 50px;
+  }
+`;
